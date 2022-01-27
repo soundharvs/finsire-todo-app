62eccc9cf43d942bacd2df769765eabdb4c05f2b
soundharvs <soundharvs2001@gmail.com>
2022-01-27 23:37:34 +0530

    database changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_27_142057) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "description"
     t.boolean "completed", default: false
